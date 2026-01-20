15ae0f30dd820e5513430692f2dee7e95ffd4b47
hankhu <hankhu@webim.com.tw>
2026-01-20 08:35:00 +0800

    feat: update Vuetify theme configuration and dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   // https://nuxt.com/modules
-  modules: ['@nuxthub/core', '@nuxt/eslint', 'vuetify-nuxt-module', '@nuxt/content', '@nuxtjs/tailwindcss'],
+  modules: ['@nuxthub/core', '@nuxt/eslint', 'vuetify-nuxt-module', '@nuxtjs/tailwindcss'],
 
   // https://devtools.nuxt.com
   devtools: { enabled: true },
